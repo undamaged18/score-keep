8a470461170a21ebe886555b7743e9c009c72fc6
Connor Cordell <clcordell93@gmail.com>
2018-09-14 16:40:51 +0100

    Inital commit

---
+++ client/main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Tracker } from 'meteor/tracker';
+
+import { Players, calculatePosition } from '../imports/api/players';
+import App from '../imports/ui/app';
+
+Meteor.startup(() => {
+    Tracker.autorun(() => {
+        let players = Players.find({}, {
+            sort: {
+                score: -1
+            }
+        }).fetch();
+        let positionedPlayers = calculatePosition(players);
+        let title = 'Score Keep App';
+        ReactDom.render(
+        <App 
+        title={title} 
+        players={positionedPlayers} />,
+        document.getElementById('app')
+        );
+    });
+});
---
+++ imports/ui/PlayerList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FlipMove from 'react-flip-move';
+
+import Player from './Player';
+
+export default class PlayerList extends React.Component {
+    renderPlayers() {
+
+        if (this.props.players.length === 0) {
+            return (
+                <div className="item">
+                <p className="item__message">Add first player to begin.</p>
+                </div>
+            )
+        } else {
+            return this.props.players.map((player) => {
+                return <Player 
+                key={player._id} 
+                player={player}
+                />
+            });
+        }
+    }
+    render() {
+        return (
+        <div>
+            <FlipMove
+            maintainContainerHeight={true}>
+                {this.renderPlayers()}
+            </FlipMove>
+        </div>
+        )
+    }
+}
+
+PlayerList.propTypes = {
+    players: PropTypes.array.isRequired
+}
